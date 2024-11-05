93684cb35ac44c0dadf92b209f3fd28cc61906d0
Harkirat Kaur <kkirat911@gmail.com>
2024-11-05 13:18:52 -0330

    circles FrameMotion

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
 
 type Props = {}
 
 function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hey Fam! You found me— Harkirat Kaur!",
+            "// ChaserOfNewFrontiers.jsx",
+            "<ExplorerOfIdeas, <BuilderOfSolutions />"
+        ],
+        loop: true,
+        delaySpeed: 900
+    })
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles/>
+        <h1>
+        <span>{text}</span> {/*React simple typewriter*/}
+        <Cursor cursorColor='#F7AB0A' cursorBlinking={true} cursorStyle='!'/>
+        </h1>
+            
+
+            
+        
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>Personal Portfolio</title>
         
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
       {/* <Contact /> */}
 
       {/* <Footer /> */}
-    </>
+    </div>
   );
 };
 
