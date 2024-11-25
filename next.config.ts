bd07d19fe316406e4c790430b7cf5729b1d01255
Harkirat Kaur <kkirat911@gmail.com>
2024-11-25 14:40:03 -0330

    sanity set up

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  },
 };
+
+console.log("Sanity project ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+console.log("Sanity dataset:", process.env.NEXT_PUBLIC_SANITY_DATASET);
+console.log("Sanity API version:", process.env.NEXT_PUBLIC_SANITY_API_VERSION);
 
 export default nextConfig;
