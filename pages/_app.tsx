3f7920fc03c0b26b292c5dceaf6d059905751fc7
Harkirat Kaur <kkirat911@gmail.com>
2025-03-29 17:37:35 -0230

    Kasam se alst check in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import { useEffect } from 'react';
 import { init } from '@emailjs/browser';
 
